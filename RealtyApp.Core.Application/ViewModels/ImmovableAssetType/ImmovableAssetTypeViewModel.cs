9a28122f661228a42d80f9350e9e61f1d54ea7dc
Edwin Roman <romanseberino@gmail.com>
2022-08-08 16:55:29 -0400

    ImmovableAssetType ViewModel

---
+++ RealtyApp.Core.Application/ViewModels/ImmovableAssetType/ImmovableAssetTypeViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealtyApp.Core.Application.ViewModels.ImmovableAssetType
+{
+    public class ImmovableAssetTypeViewModel
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        //Navigation Properties
+        //public ICollection<ImmovableAsset> ImmovableAssets { get; set; }
+
+    }
+}
