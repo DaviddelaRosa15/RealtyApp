e67aa9c23617d741309a5e81aabb67ac122a7458
Edwin Roman <romanseberino@gmail.com>
2022-08-08 16:55:57 -0400

    Improvement ViewModel

---
+++ RealtyApp.Core.Application/ViewModels/Improvement/ImprovementViewModel.cs
@@ -0,0 +1,20 @@
+﻿using RealtyApp.Core.Application.ViewModels.Improvement_Immovable;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealtyApp.Core.Application.ViewModels.Improvement
+{
+    public class ImprovementViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        //Navigation Properties
+        public ICollection<Improvement_ImmovableViewModel> Improvement_Immovables { get; set; }
+
+    }
+}
