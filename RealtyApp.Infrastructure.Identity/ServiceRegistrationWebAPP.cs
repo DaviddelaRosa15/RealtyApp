62035678cd457fa991479071043fadd6e4078586
Edwin Roman <romanseberino@gmail.com>
2022-08-09 11:35:45 -0400

    SOLID Single Responsibility principle
    The reason here is that we area working in a class that is doing it responsibility that is setting up the services of the identity to the dependency injection collector. But in this single class we have the configuration for both presentation environment (WebAPI & WebAPP) and we area managing the configuration for authentication and authorization but I think it should be separated into different classes that each of them manage their security configurations.

---
+++ RealtyApp.Infrastructure.Identity/ServiceRegistrationWebAPP.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using RealtyApp.Core.Application.Dtos.Account;
+using RealtyApp.Core.Application.Interfaces.Repositories;
+using RealtyApp.Core.Application.Interfaces.Services;
+using RealtyApp.Core.Domain.Settings;
+using RealtyApp.Infrastructure.Identity.Entities;
+using RealtyApp.Infrastructure.Identity.Services;
+using RealtyApp.Infrastructure.Persistence.Contexts;
+using System;
+using System.Text;
+
+namespace RealtyApp.Infrastructure.Identity
+{
+    //Extension Method - Decorator
+    public static class ServiceRegistrationWebAPP
+    {
+        public static void AddIdentityInfrastructureWebAPP(this IServiceCollection services, IConfiguration configuration)
+        {
+            #region Contexts
+
+                if (configuration.GetValue<bool>("UseInMemoryDatabase"))
+                {
+                    services.AddDbContext<IdentityContext>(options => options.UseInMemoryDatabase("IdentityDb"));
+                }
+                else
+                {
+                    services.AddDbContext<IdentityContext>(options =>
+                    {
+                        options.EnableSensitiveDataLogging();
+                        options.UseSqlServer(configuration.GetConnectionString("IdentityConnection"),
+                        m => m.MigrationsAssembly(typeof(IdentityContext).Assembly.FullName));
+                    });
+                }
+
+            #endregion
+
+            #region Identity
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<IdentityContext>()
+                .AddDefaultTokenProviders();
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = "/User";
+                options.AccessDeniedPath = "/User/AccessDenied";
+            });
+
+            
+            services.AddAuthentication();
+
+            #endregion
+
+            #region Services
+                services.AddTransient<IAccountService, AccountService>();
+            #endregion
+
+        }
+    }
+}
