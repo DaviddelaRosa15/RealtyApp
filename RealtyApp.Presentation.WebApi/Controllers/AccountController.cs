86a756fbd944c1dd28f351009b3a836f30d96d01
Edwin Roman <romanseberino@gmail.com>
2022-08-09 15:03:06 -0400

    Registration Controller for Devs and Admins

--- RealtyApp.Presentation.WebApi/Controllers/AccountController.cs
+++ RealtyApp.Presentation.WebApi/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using RealtyApp.Core.Application.Dtos.Account;
 using RealtyApp.Core.Application.Interfaces.Services;
 using Swashbuckle.AspNetCore.Annotations;
+using System.Linq;
 using System.Net.Mime;
 using System.Threading.Tasks;
 
@@ -31,17 +32,81 @@ namespace RealtyApp.Presentation.WebApi.Controllers
             return Ok(await _accountService.AuthenticateAsync(request));
         }
 
-        [HttpPost("register")]
+        #region [Developer & Administrator Registration]
+
+        [HttpPost("Register/Developer")]
         [SwaggerOperation(
-            Summary = "Creacion de usuario basico",
-            Description = "Recibe los parametros necesarios para crear un usuario con el rol basico"
+            Summary = "Creación de usuario tipo Desarrollador.",
+            Description = "Recibe los parametros necesarios para crear un usuario con el rol desarrollador."
         )]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(RegisterResponse))]
         [Consumes(MediaTypeNames.Application.Json)]
-        public async Task<IActionResult> RegisterAsync([FromBody] RegisterRequest request)
+        public async Task<IActionResult> RegisterDeveloperAsync([FromBody] RegisterRequest request)
         {
-            var origin = Request.Headers["origin"];
-            return Ok(await _accountService.RegisterBasicUserAsync(request, origin));
+            try
+            {
+                var origin = Request.Headers["origin"];
+                RegisterResponse result = await _accountService.RegisterDeveloperUserAsync(request, origin);
+
+               
+                if (result.HasError || !ModelState.IsValid)
+                {
+                    result.Error += string.Join(" | ", ModelState.Values
+                    .SelectMany(v => v.Errors)
+                    .Select(e => e.ErrorMessage));
+
+                    return BadRequest(result);
+                }
+
+
+                return StatusCode(StatusCodes.Status201Created);
+
+            }
+            catch (System.Exception e)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest);
+            }
+
         }
+
+
+        [HttpPost("Register/Administrator")]
+        [SwaggerOperation(
+            Summary = "Creación de usuario tipo administrador.",
+            Description = "Recibe los parametros necesarios para crear un usuario con el rol administrador."
+        )]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(RegisterResponse))]
+        [Consumes(MediaTypeNames.Application.Json)]
+        public async Task<IActionResult> RegisterAdministratorAsync([FromBody] RegisterRequest request)
+        {
+            try
+            {
+                var origin = Request.Headers["origin"];
+                var result = await _accountService.RegisterAdministratorUserAsync(request, origin);
+
+                if (result.HasError || !ModelState.IsValid)
+                {
+                    result.Error += string.Join(" | ", ModelState.Values
+                    .SelectMany(v => v.Errors)
+                    .Select(e => e.ErrorMessage));
+
+                    return BadRequest(result);
+                }
+
+                return StatusCode(StatusCodes.Status201Created);
+
+            }
+            catch (System.Exception e)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest);
+            }
+
+        }
+
+        #endregion
+
 
         [HttpGet("confirm-email")]
         [SwaggerOperation(
