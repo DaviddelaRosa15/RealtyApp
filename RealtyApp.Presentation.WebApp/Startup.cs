7cc98acf89b7cba34c1a2832f9d0f355b717691c
David N. de la Rosa L <89267615+DaviddelaRosa15@users.noreply.github.com>
2022-08-09 19:33:41 -0400

    Agregue todos los links de navegación necesarios para el proyecto.

--- RealtyApp.Presentation.WebApp/Startup.cs
+++ RealtyApp.Presentation.WebApp/Startup.cs
@@ -61,7 +61,7 @@ namespace RealtyApp.Presentation.WebApp
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=User}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
