4778c4e6567f956014c5babbd046e57ed2dcbf85
David N. de la Rosa L <89267615+DaviddelaRosa15@users.noreply.github.com>
2022-08-10 09:55:02 -0400

    Controlador admin

---
+++ RealtyApp.Presentation.WebApp/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealtyApp.Presentation.WebApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
