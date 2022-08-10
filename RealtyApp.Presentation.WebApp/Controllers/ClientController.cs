5b3bd61c9deedcfa82d87deaa0dd676f56b6b1db
Alexander Polanco <alexanderrpolanco11@gmail.com>
2022-08-10 09:03:43 -0400

    Metodos de accountServices y UserServices, listo para gestionar usuarios tipo: cliente y agente

--- RealtyApp.Presentation.WebApp/Controllers/ClientController.cs
+++ RealtyApp.Presentation.WebApp/Controllers/ClientController.cs
@@ -6,128 +6,41 @@ using RealtyApp.Core.Application.Interfaces.Services;
 using RealtyApp.Core.Application.Dtos.Account;
 using RealtyApp.Presentation.WebApp.Middlewares;
 using RealtyApp.Core.Application.Enums;
+using System.IO;
 
 namespace RealtyApp.Presentation.WebApp.Controllers
 {
-    public class UserController : Controller
+    public class ClientController : Controller
     {
         private readonly IUserService _userService;
-
-        public UserController(IUserService userService)
+        public ClientController(IUserService userService)
         {
             _userService = userService;
         }
 
         [ServiceFilter(typeof(LoginAuthorize))]
-        public IActionResult Index()
+        public async Task<IActionResult> DeleteClient(string id)
         {
-            return View(new LoginViewModel());
-        }
+            await _userService.DeleteAsync(id);
+            string basePath = $"/Images/Client/{id}";
+            string path = Path.Combine(Directory.GetCurrentDirectory(), $"wwwroot{basePath}");
 
-        [ServiceFilter(typeof(LoginAuthorize))]
-        [HttpPost]
-        public async Task<IActionResult> Index(LoginViewModel vm)
-        {
-            if (!ModelState.IsValid)
+            if (Directory.Exists(path))
             {
-                return View(vm);
-            }
+                DirectoryInfo directory = new(path);
 
-            AuthenticationResponse userVm = await _userService.LoginAsync(vm);
-            if (userVm != null && userVm.HasError != true)
-            {
-                HttpContext.Session.Set<AuthenticationResponse>("user", userVm);
-                if (userVm.Roles.Contains(Roles.Agent.ToString()))
+                foreach (FileInfo file in directory.GetFiles())
                 {
-                    return RedirectToRoute(new { controller = "Agent", action = "Index" });
+                    file.Delete();
                 }
-                return RedirectToRoute(new { controller = "Home", action = "Index" });
+                foreach (DirectoryInfo folder in directory.GetDirectories())
+                {
+                    folder.Delete(true);
+                }
+
+                Directory.Delete(path);
             }
-            else
-            {
-                vm.HasError = userVm.HasError;
-                vm.Error = userVm.Error;
-                return View(vm);
-            }
-        }
-
-        
-        public async Task<IActionResult> LogOut()
-        {
-            await _userService.SignOutAsync();
-            HttpContext.Session.Remove("user");
-            return RedirectToRoute(new { controller = "User", action = "Index" });
-        }
-
-        [ServiceFilter(typeof(LoginAuthorize))]
-        public IActionResult Register()
-        {
-            return View(new SaveUserViewModel());
-        }
-
-        [ServiceFilter(typeof(LoginAuthorize))]
-        [HttpPost]
-        
-        [ServiceFilter(typeof(LoginAuthorize))]
-        public async Task<IActionResult> ConfirmEmail(string userId, string token)
-        {
-            string response = await _userService.ConfirmEmailAsync(userId, token);
-            return View("ConfirmEmail", response);
-        }
-
-        [ServiceFilter(typeof(LoginAuthorize))]
-        public IActionResult ForgotPassword()
-        {
-            return View(new ForgotPasswordViewModel());
-        }
-
-        [ServiceFilter(typeof(LoginAuthorize))]
-        [HttpPost]
-        public async Task<IActionResult> ForgotPassword(ForgotPasswordViewModel vm)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View(vm);
-            }
-            var origin = Request.Headers["origin"];
-            ForgotPasswordResponse response = await _userService.ForgotPasswordAsync(vm, origin);
-            if (response.HasError)
-            {
-                vm.HasError = response.HasError;
-                vm.Error = response.Error;
-                return View(vm);
-            }
-            return RedirectToRoute(new { controller = "User", action = "Index" });
-        }
-
-        [ServiceFilter(typeof(LoginAuthorize))]
-        public IActionResult ResetPassword(string token)
-        {
-            return View(new ResetPasswordViewModel { Token = token });
-        }
-
-        [ServiceFilter(typeof(LoginAuthorize))]
-        [HttpPost]
-        public async Task<IActionResult> ResetPassword(ResetPasswordViewModel vm)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View(vm);
-            }
-            
-            ResetPasswordResponse response = await _userService.ResetPasswordAsync(vm);
-            if (response.HasError)
-            {
-                vm.HasError = response.HasError;
-                vm.Error = response.Error;
-                return View(vm);
-            }
-            return RedirectToRoute(new { controller = "User", action = "Index" });
-        }
-
-        public IActionResult AccessDenied()
-        {
-            return View();
+            return RedirectToRoute(new { controller = "Administrator", action = "Index" });
         }
 
     }
---
+++ RealtyApp.Presentation.WebApp/Controllers/UserController.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealtyApp.Core.Application.ViewModels.User;
+using System.Threading.Tasks;
+using RealtyApp.Core.Application.Helpers;
+using RealtyApp.Core.Application.Interfaces.Services;
+using RealtyApp.Core.Application.Dtos.Account;
+using RealtyApp.Presentation.WebApp.Middlewares;
+using RealtyApp.Core.Application.Enums;
+
+namespace RealtyApp.Presentation.WebApp.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [ServiceFilter(typeof(LoginAuthorize))]
+        public IActionResult Index()
+        {
+            return View(new LoginViewModel());
+        }
+
+        [ServiceFilter(typeof(LoginAuthorize))]
+        [HttpPost]
+        public async Task<IActionResult> Index(LoginViewModel vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(vm);
+            }
+
+            AuthenticationResponse userVm = await _userService.LoginAsync(vm);
+            if (userVm != null && userVm.HasError != true)
+            {
+                HttpContext.Session.Set<AuthenticationResponse>("user", userVm);
+                if (userVm.Roles.Contains(Roles.Agent.ToString()))
+                {
+                    return RedirectToRoute(new { controller = "Agent", action = "Index" });
+                }
+                return RedirectToRoute(new { controller = "Home", action = "Index" });
+            }
+            else
+            {
+                vm.HasError = userVm.HasError;
+                vm.Error = userVm.Error;
+                return View(vm);
+            }
+        }
+
+
+        public async Task<IActionResult> LogOut()
+        {
+            await _userService.SignOutAsync();
+            HttpContext.Session.Remove("user");
+            return RedirectToRoute(new { controller = "User", action = "Index" });
+        }
+
+
+        [ServiceFilter(typeof(LoginAuthorize))]
+        public async Task<IActionResult> ConfirmEmail(string userId, string token)
+        {
+            string response = await _userService.ConfirmEmailAsync(userId, token);
+            return View("ConfirmEmail", response);
+        }
+
+        [ServiceFilter(typeof(LoginAuthorize))]
+        public IActionResult ForgotPassword()
+        {
+            return View(new ForgotPasswordViewModel());
+        }
+
+        [ServiceFilter(typeof(LoginAuthorize))]
+        [HttpPost]
+        public async Task<IActionResult> ForgotPassword(ForgotPasswordViewModel vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(vm);
+            }
+            var origin = Request.Headers["origin"];
+            ForgotPasswordResponse response = await _userService.ForgotPasswordAsync(vm, origin);
+            if (response.HasError)
+            {
+                vm.HasError = response.HasError;
+                vm.Error = response.Error;
+                return View(vm);
+            }
+            return RedirectToRoute(new { controller = "User", action = "Index" });
+        }
+
+        [ServiceFilter(typeof(LoginAuthorize))]
+        public IActionResult ResetPassword(string token)
+        {
+            return View(new ResetPasswordViewModel { Token = token });
+        }
+
+        [ServiceFilter(typeof(LoginAuthorize))]
+        [HttpPost]
+        public async Task<IActionResult> ResetPassword(ResetPasswordViewModel vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(vm);
+            }
+
+            ResetPasswordResponse response = await _userService.ResetPasswordAsync(vm);
+            if (response.HasError)
+            {
+                vm.HasError = response.HasError;
+                vm.Error = response.Error;
+                return View(vm);
+            }
+            return RedirectToRoute(new { controller = "User", action = "Index" });
+        }
+
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+
+    }
+
+}
