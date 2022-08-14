9ecaba25068f3b60534f91699ab6551ebd2f4618
Edwin Roman <romanseberino@gmail.com>
2022-08-14 16:00:49 -0400

    Basic CQRS for ImmovableAssetType

---
+++ RealtyApp.Core.Application/Features/ImmovableAssetType/Commands/CreateAssetType/CreateAssetTypeCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealtyApp.Core.Application.Features.ImmovableAssetType.Commands.CreateAssetType
+{
+    /// <summary>
+     /// Nos permite crear un nuevo tipo de inmueble, especificado la informacion descrita en la documentacion.
+    /// </summary>  
+    public class CreateAssetTypeCommand
+    {
+
+
+
+
+    }
+}
---
+++ RealtyApp.Core.Application/Features/ImmovableAssetType/Queries/GetByIdAssetType/GetImmovableAssetTypeByIdQuery.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using MediatR;
+using RealtyApp.Core.Application.Dtos.EntitiesDTOs.ImmovableAssetType;
+using RealtyApp.Core.Application.Interfaces.Repositories;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RealtyApp.Core.Application.Features.ImmovableAssetType.Queries.GetByIdAssetType
+{
+    //<summary>
+    // Permite obtener el tipo de inmueble deseado mediante el ID.
+    //</summary>
+
+
+    public class GetImmovableAssetTypeByIdQuery : IRequest<ImmovableAssetTypeDTO>
+    {
+        [SwaggerParameter(Description = "El id del inmueble que desea obtener.")]
+        public int Id { get; set; }
+    }
+
+    public class GetImmovableAssetTypeByIdHandler : IRequestHandler<GetImmovableAssetTypeByIdQuery, ImmovableAssetTypeDTO>
+    {
+        private readonly IImmovableAssetTypeRepository _assetTypeRepository;
+        private readonly IMapper _maper;
+
+        public GetImmovableAssetTypeByIdHandler(IImmovableAssetTypeRepository assetTypeRepository, IMapper maper)
+        {
+            _assetTypeRepository = assetTypeRepository;
+            _maper = maper;
+        }
+
+        public async Task<ImmovableAssetTypeDTO> Handle(GetImmovableAssetTypeByIdQuery request, CancellationToken cancellationToken)
+        {
+            var result = await GetImmovableAssetTypeDTOById(request.Id);
+            
+            if (result == null)
+                 throw new Exception($"Immovable Asset type not found.");
+            else
+                return result;
+        }
+
+        private async Task<ImmovableAssetTypeDTO> GetImmovableAssetTypeDTOById(int id)
+        {
+            var result = await _assetTypeRepository.GetByIdAsync(id);
+            
+            if (result == null)
+                return null;
+            else
+                return _maper.Map<ImmovableAssetTypeDTO>(result);
+        }
+
+
+    }
+
+
+}
