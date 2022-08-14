09c011f1947f224dfda3bb6f1334c3822062ec4d
David N. de la Rosa L <89267615+DaviddelaRosa15@users.noreply.github.com>
2022-08-14 15:47:12 -0400

    Añadiendo la propiedad que faltaba y arreglando el tipo de las cantidades de baño y habitaciones.

---
+++ RealtyApp.Infrastructure.Persistence/Migrations/20220814194527_AddingAgentId-ImmovableAsset.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RealtyApp.Infrastructure.Persistence.Migrations
+{
+    public partial class AddingAgentIdImmovableAsset : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "BedroomQuantity",
+                table: "ImmovableAssets",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BathroomQuantity",
+                table: "ImmovableAssets",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AgentId",
+                table: "ImmovableAssets",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AgentId",
+                table: "ImmovableAssets");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "BedroomQuantity",
+                table: "ImmovableAssets",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "BathroomQuantity",
+                table: "ImmovableAssets",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
