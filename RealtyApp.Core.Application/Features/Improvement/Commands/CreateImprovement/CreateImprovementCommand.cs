0df00b0c661ffecf84a49a9f8b7a87217464e28d
Edwin Roman <romanseberino@gmail.com>
2022-08-14 23:01:24 -0400

    CQRS for Improvements

---
+++ RealtyApp.Core.Application/Features/Improvement/Commands/CreateImprovement/CreateImprovementCommand.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using MediatR;
+using RealtyApp.Core.Application.Interfaces.Repositories;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RealtyApp.Core.Application.Features.Improvement.Commands.CreateImprovement
+{
+    /// <summary>
+    /// Este endpoint nos permite crear una nueva mejora en el sistema.
+    /// </summary>
+    public class CreateImprovementCommand : IRequest<int>
+    {
+        [SwaggerParameter(Description = "Opcional autogenerado, es el identificador o codigo para nuestra mejora inmobiliaria.")]
+        public int Id { get; set; }
+
+        [SwaggerParameter(Description = "Es el nombre que contendra nuestra mejora inmobiliaria", Required = true)]
+        [Required(ErrorMessage = "Debe de especificar el un nombre para esta mejora.")]
+        ///<example>Jacuzzi</example>
+        public string Name { get; set; }
+
+        [SwaggerParameter(Description = "Mas detalles sobre la mejora...", Required = true)]
+        [Required(ErrorMessage = "Debe de especificar una descripción para esta mejora.")]
+        public string Description { get; set; }
+    }
+
+
+    public class CreateImprovementCommandHandler : IRequestHandler<CreateImprovementCommand, int>
+    {
+        private readonly IImprovementRepository _improvementRepository;
+        private readonly IMapper _mapper;
+
+        public CreateImprovementCommandHandler(IImprovementRepository improvementRepository, IMapper mapper)
+        {
+            _improvementRepository = improvementRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<int> Handle(CreateImprovementCommand command, CancellationToken cancellationToken)
+        {
+
+            var valueMapped = _mapper.Map<RealtyApp.Core.Domain.Entities.Improvement>(command);
+
+            valueMapped = await _improvementRepository.AddAsync(valueMapped);
+
+            return valueMapped.Id;
+
+        }
+
+
+    }
+
+
+}
---
+++ RealtyApp.Core.Application/Features/Improvement/Queries/GetAllImprovement/GetAllImprovementQuery.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using MediatR;
+using RealtyApp.Core.Application.Dtos.EntitiesDTOs.Improvement;
+using RealtyApp.Core.Application.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RealtyApp.Core.Application.Features.Improvement.Queries.GetAllImprovement
+{
+    /// <summary>
+    /// Permite recoger todas las mejoras que estan disponibles en el sistema.
+    /// </summary>
+
+    public class GetAllImprovementQuery : IRequest<ICollection<ImprovementDTO>>
+    {
+
+    }
+
+    public class GetAllImprovementQueryHandler : IRequestHandler<GetAllImprovementQuery, ICollection<ImprovementDTO>>
+    {
+        private readonly IImprovementRepository _improvementRepository;
+        private readonly IMapper _mapper;
+
+        public GetAllImprovementQueryHandler(IImprovementRepository improvementRepository, IMapper mapper)
+        {
+            _improvementRepository = improvementRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<ICollection<ImprovementDTO>> Handle(GetAllImprovementQuery request, CancellationToken cancellationToken)
+        {
+
+            var result = await GetAllImprovementDtoWithIncludes();
+
+            if (result == null)
+                throw new Exception("No se encontro ninguna mejora en el sistema, por favor si esto persiste contactenos al soporte.");
+
+            return result;
+
+        }
+
+        private async Task<List<ImprovementDTO>> GetAllImprovementDtoWithIncludes()
+        {
+            var improvements = await _improvementRepository.GetAllWithIncludeAsync(new List<string>() { "Improvement_Immovable" });
+
+            List<ImprovementDTO> improvementViews = _mapper.Map<List<ImprovementDTO>>(improvements);
+
+            return improvementViews;
+        }
+
+    }
+
+}
