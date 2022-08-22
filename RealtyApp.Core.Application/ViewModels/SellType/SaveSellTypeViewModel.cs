09d834645aa8c4f9549edefb22672b858ad1dd71
Alexander Polanco <alexanderrpolanco11@gmail.com>
2022-08-22 16:13:37 -0400

    UI lista de Funcionalidades del Administrador

--- RealtyApp.Core.Application/ViewModels/SellType/SaveSellTypeViewModel.cs
+++ RealtyApp.Core.Application/ViewModels/SellType/SaveSellTypeViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using RealtyApp.Core.Application.ViewModels.ImmovableAsset;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,11 @@ namespace RealtyApp.Core.Application.ViewModels.SellType
     public class SaveSellTypeViewModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Debe de especificar un nombre para este tipo de venta.")]
+
         public string Name { get; set; }
+        [Required(ErrorMessage = "Debe de especificar un nombre para este tipo de venta.")]
+
         public string Description { get; set; }
         public List<ImmovableAssetViewModel> ImmovableAssets { get; set; }
     }
